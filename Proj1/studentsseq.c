e902be74e52669755ddada6df6edc7b9ccd07587
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2022-10-20 14:50:22 -0300

    updating sequential file

--- Proj1/studentsseq.c
+++ Proj1/studentsseq.c
@@ -3,70 +3,134 @@
 #include <math.h>
 // gcc studentsseq.c -o sseq -lm
 
-/* Os dados retornados
-para cada nível são: a menor e a maior nota, a mediana, a média aritmética simples, e o
-desvio padrão */
+#define BUCKET_LEN 100
+#define GRADE_MAX 101
 
-void read_entries(int *grades, int R, int C, int A) {
+typedef struct _cities {
+    int *grade_frequencies; // per city
+    int mean;
+} CITIES;
+
+typedef struct _region {
+    CITIES *city;
+    int *grade_frequencies; // per region
+    int mean;
+} REGIONS;
+
+
+/* Geração pseudo-aleatória das notas de cada aluno a partir de uma seed */
+void get_grades(int *grades, int R, int C, int A, int seed) {
+    srand(seed);
     for (int i = 0, student = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             for (int k = 0; k < A; k++) {
-                scanf("%d", &grades[student++]);
+                grades[student++] = rand() % GRADE_MAX; 
             }
         }
     }
 }
 
-int max(int a, int b) {
-    return a > b ? a : b;
-}
-
-int min(int a, int b) {
-    return a < b ? a : b;
-}
-
-int *find_min_max(int *grade, int start, int end) {
-    int min_max[2] = {-1, 101};
+/* Counting Sort */
+int *counting_sort(int *grade, int start, int end) {
+    int *bucket = calloc(BUCKET_LEN, sizeof(int));
     for (int i = start; i <= end; i++)
-        min_max[0] = min(min_max[0], grade[i]), min_max[1] = max(min_max[1], grade[i]);
-    
-    return min_max;
-}
-
-double mean(int *grade, int start, int end) {
-    int mean = 0;
-    for (int i = start; i <= end; i++)
-        mean += grade[i];
-    
-    return mean / (end - start + 1);
-}
-
-double standard_deviation(int *grade, int start, int end, double mean) {
-    int sum = 0;
-    for (int i = start; i <= end; i++)
-        sum += pow(grade[i] - mean, 2);
-
-    return sqrt(sum / (end - start + 1));
-}
-
-void bucket_sort(int *grade, int start, int end) {
-    int bucket[101] = {0};
-    for (int i = start; i <= end; i++) 
         bucket[grade[i]]++;
-    
-    for (int i = 0, j = 0; i < 101; i++) 
+
+    for (int i = 0, j = 0; i < BUCKET_LEN; i++) 
+        while(bucket[i]--) 
+            grade[j++] = i;
+
+    return bucket;
+}
+
+void add_buckets(int *f_bucket, int *s_bucket, int *result) {
+    for (int i = 0; i < BUCKET_LEN; i++)
+        result[i] = f_bucket[i] + s_bucket[i];
+}
+
+void sort_from_bucket(int *grade, int *bucket, int start, int end) {
+    for (int i = 0, j = 0; i < BUCKET_LEN; i++) 
         while(bucket[i]--) 
             grade[j++] = i;
 }
 
+/* 
+a + b + a + a + b  -> 5 iterations
+3*a + 2*b -> 2 iterations
+*/
+double mean(int *grade_freq, int num_of_elems) {
+    int mean = 0;
+    for (int i = 0; i < BUCKET_LEN; i++)
+        mean += grade_freq[i] * i;
+    
+    return mean / num_of_elems;
+}
+
+double standard_deviation(int *grade, double mean, int num_of_elems) {
+    int sum = 0;
+    for (int i = 0; i <= BUCKET_LEN; i++)
+        sum += grade[i] * pow(i - mean, 2);
+
+    return sqrt(sum / num_of_elems);
+}
+
+REGIONS *initialize_regions(int R, int C) {
+    REGIONS *br = malloc(sizeof(REGIONS) * R);
+    for (int i = 0; i < R; i++)
+        br->city = calloc(C, sizeof(CITIES));
+
+    return br;
+}
+
+/* Cada regiao tem R*C elementos (de 0 até R*c -1) */
 int main() {
     int R, C, A, seed;
     scanf("%d%d%d%d", &R, &C, &A, &seed); 
     
     int *grades = malloc(sizeof(int) *  R * C * A);
-    read_entries(grades, R, C, A);
+    get_grades(grades, R, C, A, seed);
 
-    free(grades);
+    /* Prints por cidade de regiao */
+    REGIONS *brazil = initialize_regions(R, C);
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            // (i * C * A) + (j * A) = primeira pos da regiao + primeira posicao da cidade dentro da regiao
+            int region_start = A * (i * C + j); 
+            brazil[i].city[j].grade_frequencies = counting_sort(grades, region_start, region_start + A - 1);
+
+            int min = grades[region_start];
+            int max = grades[region_start + A - 1];
+
+            int median;
+            if (!(A % 2))
+                median = (grades[region_start - 1 + (A / 2)] + grades[region_start + A / 2]) / 2;
+            else 
+                median = grades[region_start + A / 2];
+
+            int sum = 0;
+            for (int k = 0; k < BUCKET_LEN; k++)
+                sum += k * brazil[i].city[j].grade_frequencies[k];
+            brazil[i].city[j].mean = sum / A;
+
+            int sd = 0;
+            for (int k = 0; k < BUCKET_LEN; k++)
+                sd += k * pow(brazil[i].city[j].grade_frequencies[k] - brazil[i].city[j].mean, 2);
+            sd = sqrt(sd / A);
+
+            printf("Reg %d - Cid %d: menor: %d, maior: %d, mediana: %d, média: %d e DP: %d\n", i, j, min, max, median, brazil[i].city[j].mean, sd);
+        }
+    }
+    
+    /* Print por regiao */
+    for (int i = 0; i < R; i++) {
+        brazil[i].mean = 0;
+        for (int j = 0; j < C; j++) 
+            brazil[i].mean += brazil[i].city[j].mean;
+        brazil[i].mean /= C;
+
+        printf("Reg 0: menor: 10, maior: 95, mediana: 40.00, média: 43.63 e DP: 25.07");
+    }   
 
     return 0;
 }
+ 
