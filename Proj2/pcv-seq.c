787ec9b7fb48135aee5fd7d0f2c78afda9de5f9e
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2022-12-05 11:48:12 -0300

    gotta go fast

--- Proj2/pcv-seq.c
+++ Proj2/pcv-seq.c
@@ -20,45 +20,7 @@ void swap(int *list, int a, int b) {
     list[b] = old_a;
 }
 
-void heap_permutation(int *base, int size, int **perm, int n, int *idx) {
-    if (n == 1) {
-        for (int i = 0; i <= size; i++) 
-            perm[*idx][i] = base[i];
-        (*idx)++;
-        return;
-    }
-
-    for (int i = 1; i < n; i++) {
-        heap_permutation(base, size, perm, n - 1, idx);
-        if (n % 2) 
-            swap(base, 1, n - 1);
-        else    
-            swap(base, i, n - 1);
-    }
-}   
-
-int **generate_permutations(int *city, int n, int src) {
-    int max = factorial(n - 1);
-    int **permutations = malloc(max * sizeof(int *));
-
-    for (int i = 0; i < max; i++) 
-        permutations[i] = malloc((n + 1) * sizeof(int));
-
-    int *base = malloc((n + 1) * sizeof(int));
-    base[0] = base[n] = src;
-
-    for (int i = 0, j = 1; i < n; i++) 
-        if (city[i] != src) base[j++] = city[i]; 
-
-    int idx = 0;
-    heap_permutation(base, n, permutations, n, &idx);
-
-    free(base);
-    
-    return permutations;
-}
-
-int solve_tsp(int *path, int **adj_matrix, int n) {
+int get_cost(int *path, int **adj_matrix, int n) {
     int cost = 0;
     for (int i = 1; i <= n; i++)
         cost += adj_matrix[path[i-1]][path[i]];
@@ -66,15 +28,43 @@ int solve_tsp(int *path, int **adj_matrix, int n) {
     return cost;
 }
 
-void free_mem(int **adj_matrix, int *city, int **permutations, int n, int max) {
+void heap_permutation(int *base, int size, int n, BEST_PATH *optimal, int **adj_matrix) {
+    if (n == 1) {
+        int cost = get_cost(base, adj_matrix, size);
+        if (cost < optimal->cost) {
+            optimal->cost = cost;
+            for (int i = 0; i <= size; i++)
+                optimal->path[i] = base[i];
+        }
+        return;
+    }
+
+    for (int i = 1; i < n; i++) {
+        heap_permutation(base, size, n - 1, optimal, adj_matrix);
+        if (n % 2) 
+            swap(base, 1, n - 1);
+        else    
+            swap(base, i, n - 1);
+    }
+}   
+
+void solve_tsp(int *city, int **adj_matrix, int n, int src, BEST_PATH *optimal) {
+    int *base = malloc((n + 1) * sizeof(int));
+    base[0] = base[n] = src;
+
+    for (int i = 0, j = 1; i < n; i++) 
+        if (city[i] != src) base[j++] = city[i]; 
+
+    heap_permutation(base, n, n, optimal, adj_matrix);
+
+    free(base);
+}
+
+void free_mem(int **adj_matrix, int *city, int n, int max, BEST_PATH optimal) {
     for (int i = 0; i < n; i++)
         free(adj_matrix[i]);
     free(adj_matrix);
-    free(city);
-
-    for (int i = 0; i < max; i++)
-        free(permutations[i]);
-    free(permutations);
+    free(city), free(optimal.path);
 }
 
 int main() {
@@ -98,16 +88,10 @@ int main() {
     scanf("%d", &src);
 
     double seq_start = omp_get_wtime();
-    int **path = generate_permutations(city, n, src);
     
-    BEST_PATH route = {NULL, INT_MAX};
-    for (int i = 0; i < max; i++) {
-        int cost = solve_tsp(path[i], adj_matrix, n); 
-        if (route.cost > cost) {
-            route.cost = cost;
-            route.path = path[i];
-        }
-    }
+    BEST_PATH route = {malloc((n+1) * sizeof(int)), INT_MAX};
+    solve_tsp(city, adj_matrix, n, src, &route);
+    
     double seq_end = omp_get_wtime();
 
     printf("Best path: ");
@@ -117,7 +101,7 @@ int main() {
     
     printf("Tempo de resposta sem considerar E/S, em segundos: %.3fs\n", seq_end - seq_start);
     
-    free_mem(adj_matrix, city, path, n, max);
+    free_mem(adj_matrix, city, n, max, route);
 
     return 0;
 }
